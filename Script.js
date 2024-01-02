3f6011e472d98918f24860cf74cd845e6aee2733
BlancoAquatico <148373727+BlancoAquatico@users.noreply.github.com>
2024-01-02 14:42:30 -0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -4,10 +4,12 @@ var TEX = "";
 
 function Darki(){
  document.body.className = "dark";
+ document.getElementById('demo').style.background = "rgba(79, 119, 230, 0.5)";
 }
 
 function Lith(){
- document.body.className = "light";
+ document.body.className = "body";
+ document.getElementById('demo').style.background = ""
 }
 
 function Texto(clicked_id){
@@ -17,13 +19,14 @@ function Texto(clicked_id){
                tecnico em eletronica pela escola        tecnica
                Philadelpho Gouvêa Netto. Alem de ter   formação
                em diversos cursos da escola virtual      alura`;
-               document.getElementById('demo').style.border = "1px solid #000000";
+               document.getElementById('demo').style.border = "1px solid rgba(79, 119, 230)";
+
 
     }if(clicked_id == 2){
         TEX = `TCC Eletronica: NSM - Navori Shopping Machine,2023
                Link:RAMDOM.LINK.ALEATORIO.COM.BR
                 `;
-        document.getElementById('demo').style.border = "1px solid #000000";
+                document.getElementById('demo').style.border = "1px solid rgba(79, 119, 230)";
 
     }if(clicked_id==3){
         TEX = `Sou um jovem brasileiro de São José do Rio Preto,
@@ -31,10 +34,9 @@ function Texto(clicked_id){
                tecnico em eletronica pela escola        tecnica
                Philadelpho Gouvêa Netto. Alem de ter   formação
                em diversos cursos da escola virtual      alura`;
-               document.getElementById('demo').style.border = "1px solid #000000";
+               document.getElementById('demo').style.border = "1px solid rgba(79, 119, 230)";
     }
     document.getElementById('demo').innerHTML= TEX;
-    console.log(document.getElementById('demo').innerHTML);
     console.log(clicked_id);
 }
 
